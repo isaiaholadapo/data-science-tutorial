8688231dc89d878db9d7d67fed1a70e0b49fede8
Isaiah Oladapao <isaiaholadapo18@gmail.com>
2020-03-29 08:40:01 +0100

    practiced multiple linear regression using backword elimination

---
+++ machine-learning/multiple regression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 09:40:42 2020
+
+@author: Isaiah
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+#onehotencoding
+labelencoder_X = LabelEncoder()
+onehotencoder = OneHotEncoder(categorical_features= [3])
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#avoiding dummy variable trap
+X = X[:, 1:]
+
+#splitting dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#fitting training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#prediction
+y_pred = regressor.predict(X_test)
+
+#Backword elimnation
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_ols = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_ols.summary()
